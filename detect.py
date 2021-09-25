c7246cd3f1c54a2258e59dab270c6629f8461022
Arpit Jain <83284168+code-arpit@users.noreply.github.com>
2021-09-25 15:02:09 +0000

    Add files via upload

---
+++ detect.py
@@ -0,0 +1,85 @@
+import cv2 as cv
+
+class Drill():
+    def __init__(self, camera):
+        self.camera = camera
+        self.capture = cv.VideoCapture(self.camera)
+        self.cam_detected(self.capture)
+
+    def cam_detected(self, capture):
+        while True:
+            _, cam = capture.read()
+            # cam = cv.imread('pcb_drill/pic.jpeg')
+            w = cam.shape[1]
+            h = cam.shape[0]
+            detector = self.parameters()
+            keypoints = detector.detect(cam)
+            overlay = cam.copy()
+
+            #targetting circle
+            main_center_x = int(w/2)
+            main_center_y = int(h/2)
+            # print(target_y)
+            # print(main_center_x)
+            main_center_color_x = (0,0,255)
+            main_center_color_y = (0,0,255) 
+            
+            for k in keypoints:
+                target_x = int(k.pt[0])
+                target_y = int(k.pt[1])
+                cv.circle(overlay, (target_x, target_y), int(k.size/2), (0,255,0), -1)
+                cv.line(overlay, (target_x-5, target_y), (target_x+5, target_y), (0,0,255), 1)
+                cv.line(overlay, (target_x, target_y-5), (target_x, target_y+5), (0,0,255), 1)
+            
+                if target_x == main_center_x and not target_y == main_center_y:
+                    main_center_color_y = (0,255,0)
+                
+                elif not target_x == main_center_x and target_y == main_center_y:
+                    main_center_color_x = (0,255,0)
+                    
+                elif target_y == main_center_y and target_x == main_center_x:
+                    main_center_color_x = (0,255,0)
+                    main_center_color_y = (0,255,0)
+                else:
+                    main_center_color_x = (0,0,255)         
+
+            cv.circle(overlay, (main_center_x, main_center_y), 2, (0,255,0), -1)
+            cv.line(overlay, (main_center_x-40, main_center_y), (main_center_x+40, main_center_y), main_center_color_x, 2)
+            cv.line(overlay, (main_center_x, main_center_y-40), (main_center_x, main_center_y+40), main_center_color_y, 2)
+            
+            opacity = 0.7
+            cv.addWeighted(overlay, opacity, cam, 1-opacity, 0, cam)    
+            img_blobs = cv.drawKeypoints(cam, keypoints, cam, (0,255,0), cv.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+            cv.imshow('pic', cam)
+
+            if cv.waitKey(1) & 0xFF==ord('q'):
+                break 
+        
+    def parameters(self):
+        #set up the detector with default parameters
+        params = cv.SimpleBlobDetector_Params()
+
+        #Defining threshold
+        params.minThreshold = 0
+        params.maxThreshold = 255
+        
+        #Filtering by area
+        params.filterByArea = True
+        params.minArea = 200
+        params.maxArea = 1000
+
+        #Filtering by color
+        params.filterByColor = False
+        # params.blobColor = 1
+
+        #Filtering by circularity
+        params.filterByCircularity = True
+        params.minCircularity = 0.3
+        params.maxCircularity = 1
+
+        #detector object created after initializing the paramters
+        detector = cv.SimpleBlobDetector_create(params)
+        return detector
+
+camera = 0
+detect = Drill(camera)
---
+++ ui.py
@@ -0,0 +1,58 @@
+import tkinter as tk
+import tkinter.font as tkFont
+import cv2 as cv
+from PIL import Image, ImageTk
+
+class App:
+    def __init__(self, root, cam=0):
+        #setting title
+        self.cam=cam
+        self.root = root
+        self.root.title("Pcb Drill Guider")
+        self.root.background_color='#f0f0f0'
+        #setting window size
+        self.screen_width = self.root.winfo_screenwidth()
+        self.screen_height = self.root.winfo_screenheight()
+        print(self.screen_width, self.screen_height)
+        self.root.geometry(f'{self.screen_width}x{self.screen_height}')
+        self.root.resizable(width=False, height=False)
+        # Getting camera ready
+        self.video = cv.VideoCapture(self.cam)
+        self.lmain = tk.Label(self.root)
+        self.lmain.place(x=20,y=20,width=self.screen_width*0.75,height=self.screen_height)
+        if self.video.isOpened():
+            self.video_stream()
+        else:
+            print("Please Select a Valid Camera")
+
+        exit_button=tk.Button(self.root)
+        exit_button["bg"] = "red"
+        ft = tkFont.Font(family='Times',size=10)
+        exit_button["font"] = ft
+        exit_button["fg"] = "#000000"
+        exit_button["justify"] = "center"
+        exit_button["text"] = "Exit"
+        exit_button.place(x=self.screen_width-100,y=self.screen_height-110,width=69,height=30)
+        exit_button["command"] = self.exit_b
+
+    def video_stream(self):
+        #getting latest frame and convert into image
+        video = cv.resize(self.video.read()[1], (int(self.screen_width*0.75), int(self.screen_height*0.8)))
+        cv2image= cv.cvtColor(video,cv.COLOR_BGR2RGB)
+        img = Image.fromarray(cv2image)
+        # Convert image to PhotoImage
+        imgtk = ImageTk.PhotoImage(image = img)
+        self.lmain.imgtk = imgtk
+        self.lmain.configure(image=imgtk)
+        # Repeat after an interval to capture continiously
+        self.lmain.after(1, self.video_stream)
+
+    def exit_b(self):
+        # print("command")
+        root.destroy()
+
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = App(root)
+    root.mainloop()
